cd1dd1ff9819528a5e19e315193870c3e29241ce
Gork <69741372+Gork336@users.noreply.github.com>
2025-04-20 14:10:46 +0800

    user analyse page

--- web/vue-vben-admin/apps/web-ele/src/views/demos/userAnalyse/index.vue
+++ web/vue-vben-admin/apps/web-ele/src/views/demos/userAnalyse/index.vue
@@ -13,6 +13,12 @@ import {
   ElStep,
   ElImage,
   ElAffix,
+  ElProgress,
+  ElAvatar,
+  ElTable,
+  ElTableColumn,
+  ElDescriptions,
+  ElDescriptionsItem,
 } from 'element-plus';
 const userProfile = reactive({
   sec_uid: 'MS4wLjABAAAALxPAZM7qgk5yrE_L-Qu4eZW_L2MJ-ApSH6yXNdoOShU',
@@ -52,25 +58,64 @@ async function onSubmit(secUid: string) {
 }
 
 import ApiDemoTable from './ApiDemoTable.vue';
+//加载状态
 const profileCardLoading = ref(false);
-function changeProfileCardLoading() {
-  profileCardLoading.value = !profileCardLoading.value;
-}
 const analyseCardLoading = ref(false);
-async function changeAnalyseCardLoading() {
-  analyseCardLoading.value = !analyseCardLoading.value;
+const similalCardLoading = ref(false);
+function changeProfileCardLoading(value = !profileCardLoading.value) {
+  profileCardLoading.value = value;
 }
-const analyseText = ref('');
+function changeAnalyseCardLoading(value = !analyseCardLoading.value) {
+  analyseCardLoading.value = value;
+}
+function changeSimilarCardLoading(value = !similalCardLoading.value) {
+  similalCardLoading.value = value;
+}
+const lossInfo = ref();
 async function handleAnalyse(sec_uid: string) {
   setActiveValue(2);
-  changeAnalyseCardLoading();
+  changeAnalyseCardLoading(true);
   let data = await requestClient.post('/userAnalyse/getRank', {
     sec_uid: sec_uid,
   });
   console.log(data);
-  analyseText.value = '重构误差：' + data.loss;
-  changeAnalyseCardLoading();
+  changeAnalyseCardLoading(false);
+  percentage.value = data.anomaly_score;
+  lossInfo.value = data.loss;
   setActiveValue(3);
+}
+const percentage = ref(0);
+const colors = [
+  { color: '#f56c6c', percentage: 20 },
+  { color: '#e6a23c', percentage: 40 },
+  { color: '#5cb87a', percentage: 60 },
+  { color: '#1989fa', percentage: 80 },
+  { color: '#6f7ad3', percentage: 100 },
+];
+
+const similarCluster = ref();
+async function getSimilarCluster() {
+  let data = await requestClient.post('/userAnalyse/similarCluster', {
+    sec_uid: userProfile.sec_uid,
+  });
+  // console.log(data);
+  similarCluster.value = data.similarCluster;
+  return 'finish';
+}
+const similarUser = ref();
+async function getSimilarUser() {
+  let data = await requestClient.post('/userAnalyse/similarUser', {
+    sec_uid: userProfile.sec_uid,
+  });
+  // console.log(data);
+  similarUser.value = data.similarUser;
+  return 'finish';
+}
+async function getSimilar() {
+  changeSimilarCardLoading(true);
+  let r1 = await getSimilarCluster();
+  let r2 = await getSimilarUser();
+  if (r1 == 'finish' && r2 == 'finish') changeSimilarCardLoading(false);
 }
 </script>
 <template>
@@ -156,16 +201,78 @@ async function handleAnalyse(sec_uid: string) {
             </div>
           </el-form-item> </el-form></el-card
       ><el-card class="w-screen"
-        ><template #header> 3 </template>
-        <div v-loading="analyseCardLoading">
-          <p v-show="analyseText == ''">模型处理中</p>
-          <p>{{ analyseText }}</p>
+        ><template #header> 异常检测 </template>
+        <div v-loading="analyseCardLoading" class="flex flex-nowrap gap-5">
+          <div class="flex flex-1 items-center justify-center">
+            <el-descriptions title="" :border="true" :column="1">
+              <el-descriptions-item label="本次样本重构误差">{{
+                lossInfo
+              }}</el-descriptions-item>
+              <el-descriptions-item label="异常分数说明"
+                >重构损失越大，异常分数越大，越有可能是异常用户</el-descriptions-item
+              >
+            </el-descriptions>
+          </div>
+          <div class="flex flex-1 items-center justify-center">
+            <el-progress
+              type="dashboard"
+              :percentage="percentage"
+              :color="colors"
+            >
+              <template #default="{ percentage }">
+                <span class="percentage-value block text-xl font-bold"
+                  >{{ percentage }}%</span
+                >
+                <span class="percentage-label mt-2 block">异常分数</span>
+              </template></el-progress
+            >
+          </div>
         </div>
+
         <template #footer>
           <el-button type="primary" @click="handleAnalyse(userProfile.sec_uid)"
-            >开始分析</el-button
+            >开始异常分析</el-button
           ></template
         >
+      </el-card>
+      <el-card class="w-screen">
+        <template #header>相似集群以及相似用户 </template>
+
+        <div class="flex flex-nowrap gap-5" v-loading="similalCardLoading">
+          <div class="flex-1">
+            <el-table :data="similarCluster">
+              <el-table-column prop="cluster_id" label="集群" width="120px" />
+
+              <el-table-column prop="avatar_list" label="包括用户">
+                <template #default="scope">
+                  <div class="-space-x-2">
+                    <el-avatar
+                      v-for="item in scope.row.avatar_list"
+                      :src="item"
+                    ></el-avatar>
+                  </div>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+          <div class="relative">
+            <div class="absolute inset-y-0 left-0 w-px bg-gray-200"></div>
+          </div>
+          <div class="flex-1">
+            <el-table :data="similarUser">
+              <el-table-column prop="avatar_medium" label="用户头像">
+                <template #default="scope">
+                  <el-avatar :src="scope.row.avatar_medium"></el-avatar>
+                </template>
+              </el-table-column>
+              <el-table-column prop="nickname" label="昵称" />
+              <el-table-column prop="similarity" label="相似性" />
+            </el-table>
+          </div>
+        </div>
+        <template #footer>
+          <el-button @click="getSimilar" type="primary">相似分析</el-button>
+        </template>
       </el-card>
     </div>
   </Page>
